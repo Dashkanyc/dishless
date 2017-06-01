b0ac9c0f04e64d955ad9664ddbe0dbd5c4d2e316
dashkanyc <daria.maccluskey@gmail.com>
2017-05-31 17:50:48 -0700

    dot dot

--- server.js
+++ server.js
@@ -12,24 +12,7 @@ var bodyParser = require('body-parser');
 // Routes, Body Parsers
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
-// This route is only to test
-app.get('/getresourcefromredis/:id', function(req, res) {
-    redisClient.get(req.params.id, function(err, val) {
-        if (err) {
-            return res.send(err)
-        }
-        return res.send(val);
-    });
-});
-app.post('/getresourcefromredis/:id', function(req, res) {
-    console.log(req.params.id);
-    redisClient.get(req.params.id, function(err, val) {
-        if (err) {
-            return res.send(err)
-        }
-        return res.send(val);
-    });
-});
+
 
 app.get('/hello',function(req,res){return res.send('Hello world!!!')});
 
